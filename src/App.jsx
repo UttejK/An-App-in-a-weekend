438b0c8508ecc2fb9831cbd9ca4b159028ace4d7
UttejK <uttej09092001@gmail.com>
2023-04-03 18:43:16 +0530

    v008

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 function App() {
   return (
-    <header class="header">
-      <div class="logo">
-        <img src="/logo.png" alt="Today I Learned - Logo" />
-        <h1>Today I Learned!</h1>
-      </div>
-      <button class="btn btn-large btn-open" type="button">
-        Share a fact
-      </button>
-    </header>
+    <>
+      <header className="header">
+        <div className="logo">
+          <img src="/logo.png" alt="Today I Learned - Logo" />
+          <h1>Today I Learned!</h1>
+        </div>
+        <button className="btn btn-large btn-open" type="button">
+          Share a fact
+        </button>
+      </header>
+      <NewFactForm />
+      <CategoryFilter />
+    </>
   );
+}
+
+function NewFactForm() {
+  return (
+    <form action="" className="fact-form">
+      fact form
+    </form>
+  );
+}
+
+function CategoryFilter() {
+  return <aside>Category filter</aside>;
+}
+
+function FactList() {
+  return <section>facts list</section>;
 }
 
 export default App;

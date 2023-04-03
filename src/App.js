fb8bd390341fa4af3873ebc8eda41d13ff0ef800
UttejK <uttej09092001@gmail.com>
2023-04-03 14:51:33 +0530

    migrate to vite

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import "./style.css";
-
-function App() {
-  return;
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <header class="header">
+      <div class="logo">
+        <img src="/logo.png" alt="Today I Learned - Logo" />
+        <h1>Today I Learned!</h1>
+      </div>
+      <button class="btn btn-large btn-open" type="button">
+        Share a fact
+      </button>
+    </header>
+  );
+}
+
+export default App;

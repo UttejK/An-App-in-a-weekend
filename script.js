f8a0abfeac22bb67c002dcc9bb11fb5706746763
UttejK <uttej09092001@gmail.com>
2023-04-03 10:48:38 +0530

    v001

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ btn.addEventListener("click", function () {
     btn.textContent = "share a fact";
   }
 });
+
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+
+  if (age >= 0) return age;
+  else return "Impossible year!!!";
+}
+
+years = calcFactAge(2012);
+const str = `This fact is "${years} years" old.`;
+console.log(str);
+
+//Arrow Functions
+
+const calcFactAge2 = (year) =>
+  year <= new Date().getFullYear()
+    ? new Date().getFullYear() - year
+    : `Impossible year. Year needs to be less or equal to ${new Date().getFullYear()}`;
+console.log(calcFactAge2(2012));
